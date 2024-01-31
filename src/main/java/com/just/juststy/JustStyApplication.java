6a6a6bacedd372fcbf9ead41a03aea9064ebb12a
khasanjontuychiboev <khasanjontuychiboev@gmail.com>
2024-01-31 14:43:09 +0500

    first commit

---
+++ src/main/java/com/just/juststy/JustStyApplication.java
@@ -0,0 +1,13 @@
+package com.just.juststy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JustStyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JustStyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/just/juststy/JustStyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.just.juststy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JustStyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
