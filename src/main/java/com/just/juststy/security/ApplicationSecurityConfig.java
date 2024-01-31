be2721415890f195c439bead395f9ce630568924
khasanjontuychiboev <khasanjontuychiboev@gmail.com>
2024-01-31 15:01:54 +0500

    second commit

--- src/main/java/com/just/juststy/security/ApplicationSecurityConfig.java
+++ src/main/java/com/just/juststy/security/ApplicationSecurityConfig.java
@@ -1,24 +1,60 @@
 package com.just.juststy.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+import static com.just.juststy.entity.ApplicationUserRole.*;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
+
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "index", "/css/*", "/js/*")
-                .permitAll()
+                .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
+                .antMatchers("/api/**").hasRole(STUDENT.name())
                 .anyRequest()
                 .authenticated()
                 .and()
-                .httpBasic();
+                .httpBasic(); // Basic Auth to be used
+    }
+
+
+    @Override
+    @Bean
+    protected UserDetailsService userDetailsService() {
+        UserDetails std1 = User.builder()
+                .username("student1")
+                .password(passwordEncoder.encode("pass"))
+                .roles(STUDENT.name())
+                .build();
+
+        UserDetails std2 = User.builder()
+                .username("student2")
+                .password(passwordEncoder.encode("pass012"))
+                .roles(ADMIN.name())
+                .build();
+
+        return new InMemoryUserDetailsManager(std1, std2);
     }
 
 }
